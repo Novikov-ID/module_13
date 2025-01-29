701454784553ecd7ba429f337193cced3d607207
Novikov-ID <novikov.id@gmail.com>
2025-01-30 01:03:50 +0300

    Update module_13_5.py

--- module_13_5.py
+++ module_13_5.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 api = ''
 bot = Bot(token=api)
-ds = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 button1 = KeyboardButton(text='Информация')
